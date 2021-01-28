75683bbf3b1ad1528de1a5c955a1b90198a8f04f
AruDesh <abdesh@gmail.com>
2021-01-28 20:50:06 +0530

    Deleting build_sh.yml

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void depositAnAmount() {
     }
 }
